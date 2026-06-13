8e495e75ccb7d1461658d1ccef0e2bb339e484a3
Wesley Medeiros <Wesley.coelho@outlook.com>
2026-06-13 15:37:43 -0300

    :recycle: refactor: Moving db connection to a separate file

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+from database import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"  # noqa: S105
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 @app.route("/hello-world", methods=["GET"])
@@ -15,3 +16,8 @@ def hello_world():
 
 if __name__ == "__main__":
   app.run(debug=True, port=3333)  # noqa: S201
+
+# Comando "flask shell" no terminal, para entrar na aplicação que esta rodando, para criar o banco de dados
+## dentro da aplicação pelo terminal, digitar "db.create_all()"
+## toda vez que um banco de dados é acessado, é criado uma sessão e essa deve ser commitada "db.session.commit()"
+## para sair da aplicação pelo terminal, digitar "exit()"
