01d26bbc923670c90ed044f268197309c96d1bb7
Wesley Medeiros <Wesley.coelho@outlook.com>
2026-06-13 17:18:47 -0300

    :tada: feat: Get User route

--- app.py
+++ app.py
@@ -66,6 +66,17 @@ def create_user():
   return jsonify({"message": "Credenciais inválidas"}), 400
 
 
+@app.route("/user/<int:id_user>", methods=["GET"])
+@login_required
+def read_user(id_user: int):
+  user = User.query.get(id_user)
+
+  if user:
+    return {"username": user.username}
+
+  return jsonify({"message": "Usuário não encontrado"}), 404
+
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "Hello world"
